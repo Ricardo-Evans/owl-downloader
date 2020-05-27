3c5f0f7c2d4c8a82b119dcd630e817af33fb11d7
daqige <daqige@sjtu.edu.cn>
2020-05-27 15:03:05 +0800

    Support https now

--- main/src/com/owl/downloader/core/HttpTask.java
+++ main/src/com/owl/downloader/core/HttpTask.java
@@ -2,14 +2,23 @@ package com.owl.downloader.core;
 
 import com.owl.downloader.io.IOCallback;
 import com.owl.downloader.io.IOScheduler;
+import com.owl.downloader.util.MyX509TrustManager;
+import com.owl.downloader.util.SSLEngineUtil;
 
+import javax.net.ssl.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.net.*;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URI;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
@@ -88,9 +97,19 @@ public class HttpTask extends BaseTask implements Task {
     @Override
     public void run() {
         if (protocol.equals("http")) {
-            setHttpFileAttributes();
+            try {
+                setHttpFileAttributes();
+            } catch (IOException e) {
+                changeStatus(Status.ERROR);
+                return;
+            }
         } else {
-            setHttpsFileAttributes();
+            try {
+                setHttpsFileAttributes();
+            } catch (Exception e) {
+                changeStatus(Status.ERROR);
+                return;
+            }
         }
 
         createFile();
@@ -117,7 +136,7 @@ public class HttpTask extends BaseTask implements Task {
     /**
      * Set Http source file's length and type.
      */
-    private void setHttpFileAttributes() {
+    private void setHttpFileAttributes() throws IOException {
         HttpURLConnection httpConnection = null;
         try {
             httpConnection = (HttpURLConnection) this.uri.toURL().openConnection(proxy);
@@ -125,12 +144,28 @@ public class HttpTask extends BaseTask implements Task {
             changeStatus(Status.ERROR);
             return;
         }
+        httpConnection.connect();
         totalLength = httpConnection.getContentLength();
         type = httpConnection.getContentType();
     }
 
-    private void setHttpsFileAttributes() {
-
+    private void setHttpsFileAttributes() throws NoSuchProviderException, NoSuchAlgorithmException, KeyManagementException, IOException {
+        SSLContext sslcontext = SSLContext.getInstance("SSL", "SunJSSE");
+        sslcontext.init(null, new TrustManager[]{new MyX509TrustManager()}, new java.security.SecureRandom());
+        HostnameVerifier ignoreHostnameVerifier = (s, sslsession) -> true;
+        HttpsURLConnection.setDefaultHostnameVerifier(ignoreHostnameVerifier);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslcontext.getSocketFactory());
+        HttpsURLConnection httpsConnection = null;
+        try {
+            httpsConnection = (HttpsURLConnection) this.uri.toURL().openConnection(proxy);
+        } catch (IOException e) {
+            changeStatus(Status.ERROR);
+            return;
+        }
+        httpsConnection.setInstanceFollowRedirects(false);
+        httpsConnection.connect();
+        totalLength = httpsConnection.getContentLength();
+        type = httpsConnection.getContentType();
     }
 
     /**
@@ -140,9 +175,8 @@ public class HttpTask extends BaseTask implements Task {
         try (SocketChannel socketChannel = SocketChannel.open()) {
             socketChannel.configureBlocking(false);
 
-
             String host = uri.getHost();
-            String fileUrl = uri.getPath();
+            String Path = uri.getPath();
             int port = uri.getPort();
             if (port == -1) {
                 port = 80;
@@ -150,57 +184,113 @@ public class HttpTask extends BaseTask implements Task {
             InetSocketAddress address = new InetSocketAddress(host, port);
             socketChannel.connect(address);
 
-            String requestMessage = ("GET " + fileUrl + " HTTP/1.1\r\n") +
+            String requestMessage = ("GET " + Path + " HTTP/1.1\r\n") +
                     "Host:" + host + "\r\n" +
                     "Connection: close\r\n" +
                     "Range: bytes=" + block.offset + "-" + (block.length + block.offset - 1) + "\r\n" +
                     "\r\n";
             ByteBuffer requestBuffer = ByteBuffer.wrap(requestMessage.getBytes());
             socketChannel.write(requestBuffer);
-            skipHeader(socketChannel);
+            skipHttpHeader(socketChannel);
 
             FileChannel fileChannel = (new FileInputStream(getDirectory() + name() + "." + type)).getChannel().position(block.offset);
             ByteBuffer responseBuffer = ByteBuffer.allocate(16 * 1024);
 
-            int readSize = 0;
-            while (readSize != -1) {
-                readSize = httpRead(socketChannel,responseBuffer);
-                httpWrite(fileChannel,responseBuffer);
-            }
+            httpRead(socketChannel, fileChannel, responseBuffer);
+
         } catch (IOException e) {
             block.available = true;
-        } finally {
-            --currentConnections;
         }
     }
 
-    private void createHttpsConnection(FileData.Block block){}
+    private void createHttpsConnection(FileData.Block block) {
+        try {
+            String host = uri.getHost();
+            String path = uri.getPath();
+            int port = uri.getPort();
+            if (port == -1) {
+                port = 443;
+            }
+            SSLEngine sslEngine = SSLEngineUtil.prepareEngine(host, port);
+            SocketChannel socketChannel = SSLEngineUtil.prepareChannel(host, port);
+            while (!socketChannel.finishConnect()) {
+            }
+            SSLSession session = sslEngine.getSession();
+            ByteBuffer myAppBuffer = ByteBuffer.allocate(session.getApplicationBufferSize());
+            ByteBuffer myNetBuffer = ByteBuffer.allocate(session.getPacketBufferSize());
+            ByteBuffer peerAppBuffer = ByteBuffer.allocate(session.getApplicationBufferSize());
+            ByteBuffer peerNetBuffer = ByteBuffer.allocate(session.getPacketBufferSize());
 
-    private int httpRead(ReadableByteChannel channel, ByteBuffer buffer){
-        final int[] readSize = new int[1];
+            SSLEngineUtil.doHandshake(socketChannel, sslEngine, myNetBuffer, peerNetBuffer);
+
+            SSLEngineUtil.sendRequest(host, port, path, sslEngine, myAppBuffer, myNetBuffer, socketChannel, block);
+
+            FileChannel fileChannel = (new FileInputStream(getDirectory() + name() + "." + type)).getChannel().position(block.offset);
+
+            httpsRead(socketChannel, fileChannel, peerNetBuffer, peerAppBuffer, sslEngine);
+        } catch (Exception e) {
+            block.available = true;
+        }
+    }
+
+
+    private void httpsRead(ReadableByteChannel readChannel, WritableByteChannel writeChannel, ByteBuffer netBuffer, ByteBuffer appBuffer, SSLEngine sslEngine) {
+        IOCallback httpsReadCallback = (Channel socketchannel, ByteBuffer responseBuffer, int size, Exception exception) -> {
+            if (size != -1) {
+                responseBuffer.flip();
+                SSLEngineResult res = sslEngine.unwrap(responseBuffer, appBuffer);
+                if (res.getStatus() == SSLEngineResult.Status.BUFFER_UNDERFLOW) {
+                    responseBuffer.compact();
+                    httpsRead((ReadableByteChannel) socketchannel, writeChannel, responseBuffer, appBuffer, sslEngine);
+                }
+                if (res.getStatus() == SSLEngineResult.Status.OK) {
+                    responseBuffer.compact();
+                    skipHttpsHeader(appBuffer);
+                    httpsWrite((ReadableByteChannel) socketchannel, writeChannel, appBuffer, appBuffer, sslEngine);
+                }
+            } else {
+                --currentConnections;
+            }
+        };
+        Objects.requireNonNull(ioScheduler).read(readChannel, netBuffer, httpsReadCallback);
+    }
+
+    private void httpsWrite(ReadableByteChannel readChannel, WritableByteChannel writeChannel, ByteBuffer netBuffer, ByteBuffer appBuffer, SSLEngine sslEngine) {
+        IOCallback httpsWriteCallback = (Channel fileChannel, ByteBuffer responseBuffer, int size, Exception exception) -> {
+            httpsRead(readChannel, (WritableByteChannel) fileChannel, responseBuffer, appBuffer, sslEngine);
+        };
+        Objects.requireNonNull(ioScheduler).write(writeChannel, appBuffer, httpsWriteCallback);
+    }
+
+
+    private void httpRead(ReadableByteChannel readChannel, WritableByteChannel writeChannel, ByteBuffer buffer) {
         IOCallback httpReadCallback = (Channel socketchannel, ByteBuffer responseBuffer, int size, Exception exception) -> {
             long lastTime = currentTime;
             currentTime = System.currentTimeMillis();
-            downloadSpeed = size / (currentTime - lastTime) * 1000 ;//B/s
+            downloadSpeed = size / (currentTime - lastTime) * 1000;//B/s
             synchronized (this) {
                 adjustDownloadedLength(size);
             }
-            readSize[0] = size;
+            if (size != -1) {
+                httpWrite((ReadableByteChannel) socketchannel, writeChannel, responseBuffer);
+            } else {
+                --currentConnections;
+            }
         };
-        Objects.requireNonNull(ioScheduler).read(channel, buffer, httpReadCallback);
-        return readSize[0];
+        Objects.requireNonNull(ioScheduler).read(readChannel, buffer, httpReadCallback);
     }
 
-    private void httpWrite(WritableByteChannel channel,ByteBuffer buffer){
-        IOCallback httpWriteCallback = (Channel socketchannel, ByteBuffer responseBuffer, int size, Exception exception) ->{
+    private void httpWrite(ReadableByteChannel readChannel, WritableByteChannel writeChannel, ByteBuffer buffer) {
+        IOCallback httpWriteCallback = (Channel fileChannel, ByteBuffer responseBuffer, int size, Exception exception) -> {
+            httpRead(readChannel, (WritableByteChannel) fileChannel, responseBuffer);
         };
-        Objects.requireNonNull(ioScheduler).write(channel,buffer,httpWriteCallback);
+        Objects.requireNonNull(ioScheduler).write(writeChannel, buffer, httpWriteCallback);
     }
 
     /**
      * Skip the header to download content.
      */
-    private void skipHeader(SocketChannel socketChannel) throws IOException {
+    private void skipHttpHeader(SocketChannel socketChannel) throws IOException {
         ByteBuffer tempBuffer = ByteBuffer.allocate(1);
         byte LastByte = 0;
         int read = socketChannel.read(tempBuffer);
@@ -214,6 +304,25 @@ public class HttpTask extends BaseTask implements Task {
         }
         tempBuffer.clear();
         socketChannel.read(tempBuffer);
+    }
+
+    private void skipHttpsHeader(ByteBuffer appBuffer) {
+        int count = 0;
+        byte lastByte = 0;
+        if ((char) appBuffer.get(0) == 'H' && (char) appBuffer.get(1) == 'T') {
+            while (true) {
+                if (lastByte == 10 && appBuffer.get(count) == 13) {
+                    count += 2;
+                    break;
+                }
+                lastByte = appBuffer.get(count);
+                count++;
+            }
+            count++;
+            appBuffer.position(count);
+        }else {
+            appBuffer.position(0);
+        }
     }
 
     /**
