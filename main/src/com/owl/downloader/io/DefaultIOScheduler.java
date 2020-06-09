b41bf1de8014b2c14cd9f62d3413cf64e147fa21
Ricardo Evans <ricardo.a.evans@gmail.com>
2020-06-09 21:51:16 +0800

    Fix: Now IOScheduler cancel the registered channel once IO operation is ready

--- main/src/com/owl/downloader/io/DefaultIOScheduler.java
+++ main/src/com/owl/downloader/io/DefaultIOScheduler.java
@@ -24,13 +24,13 @@ public class DefaultIOScheduler implements IOScheduler, Runnable {
                     Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
                     while (iterator.hasNext()) {
                         SelectionKey key = iterator.next();
-                        key.cancel();
                         Attachment attachment = (Attachment) key.attachment();
                         if (key.isReadable())
                             executor.execute(() -> doRead((ReadableByteChannel) key.channel(), attachment.buffer, attachment.callback));
                         if (key.isWritable())
                             executor.execute(() -> doWrite((WritableByteChannel) key.channel(), attachment.buffer, attachment.callback));
                         iterator.remove();
+                        key.cancel();
                     }
                 }
             } catch (IOException e) {
