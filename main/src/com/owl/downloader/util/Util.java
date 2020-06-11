2d5fcca34c9ebea0f077f40f757b1bafb0160774
Ricardo Evans <ricardo.a.evans@gmail.com>
2020-06-11 14:25:35 +0800

    Add: Speed calculation

---
+++ main/src/com/owl/downloader/util/Util.java
@@ -0,0 +1,18 @@
+package com.owl.downloader.util;
+
+public final class Util {
+    private Util() {
+    }
+
+    /**
+     * Calculate speed, in bytes/second
+     *
+     * @param delta the delta data size
+     * @param time  the time spend
+     * @param speed the speed calculated last time
+     * @return the current speed
+     */
+    public static long calculateSpeed(long delta, long time, long speed) {
+        throw new RuntimeException("not implemented yet");
+    }
+}
